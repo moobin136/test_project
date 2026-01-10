7feff4308fc4daf939ce27075d294d4676495118
moobin <moobinislam3@gamil.com>
2026-01-10 19:09:11 +0600

    add test_project

---
+++ lib/cicket_score.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: non_constant_identifier_names
+
+class CicketScore {
+  final String TeamOne;
+
+  final String TeamTow;
+
+  final String TeamTowScor;
+
+  final String TeamOneScor;
+
+  final String WingerTeam;
+  final bool isMathRuning;
+
+  CicketScore(
+      {required this.TeamOne,
+      required this.TeamTow,
+      required this.TeamTowScor,
+      required this.TeamOneScor,
+      required this.WingerTeam,
+      required this.isMathRuning});
+}
+
+// TeamOne
+// "Bangladesh"
+// (string)
+
+
+// TeamOneScor
+// 120
+// (number)
+
+
+// TeamTow
+// "England"
+// (string)
+
+
+// TeamTowScor
+// 300
+// (number)
+
+
+// WingerTeam
+// ""
+// (string)
+
+
+// isMathRuning
+// true
+// (boolean)
