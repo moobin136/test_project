e6823db179755b09f43712b50028e9c2b6023a0b
moobin136 <moobinislam3@gmail.com>
2026-02-07 10:26:55 +0600

    no change

--- lib/features/camera_view/presentation/screen/image_&_camera_path.dart
+++ lib/features/camera_view/presentation/screen/image_&_camera_path.dart
@@ -11,39 +11,41 @@ class ImageAndCameraFilePath extends GetView<ImageAndCameraFilePathController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Obx(
-        () {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 80,
-                backgroundImage: controller.filePath.value.isNotEmpty
-                    ? FileImage(File(controller.filePath.value))
-                    : null,
-                backgroundColor: Colors.grey[300],
-                child: controller.filePath.value.isEmpty
-                    ? const Icon(Icons.person, size: 80, color: Colors.white)
-                    : null,
-              ),
-              const SizedBox(height: 16),
-              TextButton(
-                child: const Text('Camera'),
-                onPressed: () {
-                  controller.imagePicToCamera();
-                },
-              ),
-              const SizedBox(height: 16),
-              TextButton(
-                child: const Text('Gallery'),
-                onPressed: () {
-                  controller.imagePicToGallery();
-                },
-              )
-            ],
-          );
-        },
-      )),
+      body: Center(
+        child: Obx(
+          () {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 80,
+                  backgroundImage: controller.filePath.value.isNotEmpty
+                      ? FileImage(File(controller.filePath.value))
+                      : null,
+                  backgroundColor: Colors.grey[300],
+                  child: controller.filePath.value.isEmpty
+                      ? const Icon(Icons.person, size: 80, color: Colors.white)
+                      : null,
+                ),
+                const SizedBox(height: 16),
+                TextButton(
+                  child: const Text('Camera'),
+                  onPressed: () {
+                    controller.imagePicToCamera();
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextButton(
+                  child: const Text('Gallery'),
+                  onPressed: () {
+                    controller.imagePicToGallery();
+                  },
+                )
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
