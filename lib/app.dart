bccc13a42cab13fca8180d3d7e521bc5d8118533
moobin136 <moobinislam3@gmail.com>
2026-02-17 09:09:17 +0600

    add file and rm old file

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:test_project/core/binder/app_binder.dart';
-import 'package:test_project/core/constant/app_colors.dart';
-import 'package:test_project/routes/routes.dart';
+import 'package:test_project/ui/screen/splash_screen.dart';
 
-class Slide extends StatelessWidget {
+import 'ui/util/app_colors.dart';
+
+class Slide extends StatefulWidget {
   const Slide({super.key});
 
   @override
+  State<Slide> createState() => _SlideState();
+}
+
+class _SlideState extends State<Slide> {
+  @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialRoute: AppRoutes.instal,
-      getPages: AppRoutes.pages,
-      debugShowCheckedModeBanner: false,
-      initialBinding: AppBinder(),
-      themeMode: ThemeMode.light,
+    return MaterialApp(
       theme: ThemeData(
-          brightness: Brightness.light,
-          scaffoldBackgroundColor: AppColors.white),
+        useMaterial3: true,
+        textTheme: const TextTheme(),
+        inputDecorationTheme: _inputDecorationTheme(),
+        elevatedButtonTheme: _elevatedButtonThemeData(),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+
+  ElevatedButtonThemeData _elevatedButtonThemeData() {
+    return ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.themColor,
+        foregroundColor: Colors.white,
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+        fixedSize: Size.fromWidth(double.maxFinite),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+
+  InputDecorationTheme _inputDecorationTheme() {
+    return InputDecorationTheme(
+      fillColor: Colors.white,
+      filled: true,
+      border: _inputBorder(),
+      errorBorder: _inputBorder(),
+      focusedBorder: _inputBorder(),
+      enabledBorder: _inputBorder(),
+    );
+  }
+
+  OutlineInputBorder _inputBorder() {
+    return OutlineInputBorder(
+      borderSide: BorderSide.none,
+      borderRadius: BorderRadius.circular(8),
     );
   }
 }
