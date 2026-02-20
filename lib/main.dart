58607e4649a16017b5b24b5d3c770c313766db43
moobin136 <moobinislam3@gmail.com>
2026-02-20 14:38:40 +0600

    clear

--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:test_project/app.dart';
-
-void main() {
-  runApp(const Slide());
-}
--- lib/ui/util/app_colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color themColor = Color(0xFF43A046);
-}
